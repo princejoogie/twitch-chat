773607fccc8b42d977603ce2fc6a9690539c1fd1
princejoogie <princejoogie@gmail.com>
2022-08-24 00:44:27 +0800

    fix: dependency list

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 import envCompatible from "vite-plugin-env-compatible";
 
 export default defineConfig({
+  server: { port: 3000 },
+  preview: { port: 3000 },
   plugins: [react(), envCompatible()],
 });
