1f63255dd2c26964548b792462d442ded3ab5f10
princejoogie <princejoogie@gmail.com>
2022-08-24 01:28:15 +0800

    chore: added simple display option

--- src/components/root/component.tsx
+++ src/components/root/component.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { ThirdPartyEmotesProvider } from "../../contexts/third-party-emotes";
 import { ChatRoot } from "../chat-root";
 import { SETTINGS } from "../../settings";
+import styles from "../../theme.module.scss";
 
 export const Root: React.FunctionComponent = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const username = (urlParams.get("username") || SETTINGS.TWITCH_LOGIN).trim();
+  const display = (urlParams.get("display") || SETTINGS.display).trim();
+  const twitchId = (urlParams.get("twitchId") || SETTINGS.TWITCH_ID).trim();
 
   return (
-    <ThirdPartyEmotesProvider channelId={SETTINGS.TWITCH_ID}>
-      <ChatRoot channelID={SETTINGS.TWITCH_ID} login={username} />
-    </ThirdPartyEmotesProvider>
+    <div className={styles[display]}>
+      <ThirdPartyEmotesProvider channelId={twitchId}>
+        <ChatRoot channelID={twitchId} login={username} />
+      </ThirdPartyEmotesProvider>
+    </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,10 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Root } from "./components/root";
 import "./index.scss";
-import { SETTINGS } from "./settings";
-import styles from "./theme.module.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className={styles[SETTINGS.theme]}>
-      <Root />
-    </div>
+    <Root />
   </React.StrictMode>,
   document.getElementById("root")
 );
